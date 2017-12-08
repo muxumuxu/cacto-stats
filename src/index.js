48d31708cd8d36d6d4fb96a87c6ac92d2f71c8d3
David Miotti <david@muxumuxu.com>
2017-12-08 14:38:52 -0800

    [+] Export into csv for all specs

--- src/index.js
+++ src/index.js
@@ -1,22 +1,58 @@
 import fs from 'fs'
 import dotenv from 'dotenv'
+import moment from 'moment'
 import Database from './database'
 
 dotenv.config()
 
 const db = new Database(process.env.DB_URI)
 
-db.connect()
-  .then(() => db.listUsers())
-  .then(users => {
-    // 1. All users
-    // 2. Users registered one week ago
-    // 3. Users with one domain and registered since at least 1 year
-    // 4. Users with more than one domain
-
-    console.log(`${users.length} users retrieved.`)
-    console.log('First -', users[0])
-    console.log('Last -', users[users.length - 1])
+const exportToCSV = (array, filename) =>
+  new Promise((resolve, reject) => {
+    const content = array.join('\n')
+    fs.writeFile(filename, content, err => {
+      if (err) return reject(err)
+      resolve(filename)
+    })
   })
+
+// 1. All users
+const exportAllUsers = users => {
+  const emails = users.map(u => u.email)
+  return exportToCSV(emails, 'output/all-users.csv')
+}
+
+// 2. Users registered two weeks ago
+const exportUserRegisteredTwoWeekAgo = users => {
+  const weekAgo = moment().subtract(14, 'd')
+  const filteredUsers = users.filter(u => moment(u.createdAt) > weekAgo)
+  const emails = filteredUsers.map(u => u.email)
+  return exportToCSV(emails, 'output/two-weeks-ago.csv')
+}
+
+// 3. Users with one domain and registered since at least 1 year.
+const exportUserRegisteredOneYearAgoWithOneDomain = users => {
+  const yearAgo = moment().subtract(1, 'y')
+  const filteredUsers = users.filter(u => moment(u.createdAt) < yearAgo && u.domains.length === 1)
+  return exportToCSV([], 'output/year-ago-with-one-domain.csv')
+}
+
+// 4. Users with more than one domain
+const exportUserWithMoreThanOneDomain = users => {
+  const filteredUsers = users.filter(u => u.domains.length > 1)
+  const emails = filteredUsers.map(u => u.email)
+  return exportToCSV(emails, 'output/more-than-one-domain.csv')
+}
+
+db.connect()
+  .then(() => db.getUsers())
+  .then(users =>
+    Promise.all([
+      exportAllUsers(users),
+      exportUserRegisteredTwoWeekAgo(users),
+      exportUserRegisteredOneYearAgoWithOneDomain(users),
+      exportUserWithMoreThanOneDomain(users)
+    ])
+  )
   .catch(console.log)
   .finally(() => db.close())
